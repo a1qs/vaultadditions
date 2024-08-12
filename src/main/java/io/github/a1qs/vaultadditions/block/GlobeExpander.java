8c6e6f6aaf5882ab0beb7ae2b3fa1929a25db759
a1qs <ankiller2007.09.20@gmail.com>
2024-08-12 20:54:33 +0200

    change use method of Globe Expander to be more readable & tooltip to mention "Expands/Decreases"

--- src/main/java/io/github/a1qs/vaultadditions/block/GlobeExpander.java
+++ src/main/java/io/github/a1qs/vaultadditions/block/GlobeExpander.java
@@ -35,20 +35,16 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class GlobeExpander extends Block {
-
     private static final VoxelShape SHAPE = Shapes.box(.1, .1, .1, .9, .9, .9);
 
     public GlobeExpander(Properties properties) {
         super(properties);
     }
-
     @Override
     public void appendHoverText(@NotNull ItemStack pStack, @javax.annotation.Nullable BlockGetter pLevel, List<Component> pTooltip, @NotNull TooltipFlag pFlag) {
         pTooltip.add(new TextComponent("Upon clicking with a Border Shard:").withStyle(ChatFormatting.YELLOW));
-        pTooltip.add(new TextComponent("Expands the World Border by a configured amount").withStyle(ChatFormatting.YELLOW));
+        pTooltip.add(new TextComponent("Expands or decreases the World Border by a configured amount").withStyle(ChatFormatting.YELLOW));
     }
-
-
 
     @Override
     public @NotNull VoxelShape getShape(BlockState state, @NotNull BlockGetter getter, @NotNull BlockPos pos, @NotNull CollisionContext context) {
@@ -57,51 +53,54 @@ public class GlobeExpander extends Block {
 
     @Override
     public @NotNull InteractionResult use(@NotNull BlockState pState, Level pLevel, @NotNull BlockPos pPos, @NotNull Player pPlayer, @NotNull InteractionHand pHand, @NotNull BlockHitResult pHit) {
-        if(!pLevel.isClientSide()) {
-            if(pPlayer.getMainHandItem().getItem() instanceof BorderShard && pHand == InteractionHand.MAIN_HAND) {
-                MinecraftServer srv = ServerLifecycleHooks.getCurrentServer();
+        if(pLevel.isClientSide()) {
+            return InteractionResult.PASS;
+        }
 
-                WorldBorder border = srv.overworld().getWorldBorder();
-                if(pLevel instanceof ServerLevel serverLevel) {
-                    WorldBorderData data = DataManager.get(serverLevel);
-                    if(pPlayer.getMainHandItem().getItem() == ModItems.BORDER_SHARD.get()) {
-                        int borderShardIncrease = CommonConfigs.BORDER_SHARD_INCREASE.get();
-                        double blocksExpanded = pPlayer.getMainHandItem().getCount() * borderShardIncrease;
-                        double newSize = border.getSize() + blocksExpanded;
-                        border.lerpSizeBetween(border.getSize(), newSize, 1000);
-                        data.setWorldBorderSize(newSize);
-                        serverLevel.playSound(null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.BEACON_POWER_SELECT, SoundSource.BLOCKS,  0.75F, 0.9F);
 
-                        if(!pPlayer.getAbilities().instabuild) {
-                            pPlayer.getMainHandItem().setCount(0);
-                        }
-                        srv.getPlayerList().broadcastMessage(new TextComponent( "[World Border] " + pPlayer.getDisplayName().getString() +  " expanded the World border by " + blocksExpanded + " Blocks!").withStyle(ChatFormatting.YELLOW), ChatType.CHAT, Util.NIL_UUID);
-                    } else {
-                        double blocksRemoved = (double) pPlayer.getMainHandItem().getCount() * CommonConfigs.INVERTED_BORDER_SHARD_DECREASE.get();
-                        double newSize = border.getSize() + blocksRemoved;
-                        if(newSize > 5) {
-                            border.lerpSizeBetween(border.getSize(), newSize, 1000);
-                            data.setWorldBorderSize(newSize);
-                            serverLevel.playSound(null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.BEACON_POWER_SELECT, SoundSource.BLOCKS,  0.75F, 0.9F);
+        if(!(pPlayer.getMainHandItem().getItem() instanceof BorderShard && pHand == InteractionHand.MAIN_HAND)) {
+            return InteractionResult.PASS;
+        }
 
-                            if(!pPlayer.getAbilities().instabuild) {
-                                pPlayer.getMainHandItem().setCount(0);
-                            }
-                            srv.getPlayerList().broadcastMessage(new TextComponent( "[World Border] " + pPlayer.getDisplayName().getString() +  " shrunk the World border by " + blocksRemoved + " Blocks!").withStyle(ChatFormatting.RED), ChatType.CHAT, Util.NIL_UUID);
+        if (!(pLevel instanceof ServerLevel serverLevel)) {
+            return InteractionResult.PASS;
+        }
 
-                        } else {
-                            srv.getPlayerList().broadcastMessage(new TextComponent( "[World Border] " + pPlayer.getDisplayName().getString() +  " attempted to shrink the border to less than 5 Blocks! ").withStyle(ChatFormatting.RED), ChatType.CHAT, Util.NIL_UUID);
-                        }
+
+        MinecraftServer srv = ServerLifecycleHooks.getCurrentServer();
+        WorldBorder border = srv.overworld().getWorldBorder();
+        WorldBorderData data = DataManager.get(serverLevel);
+
+        if(pPlayer.getMainHandItem().getItem() == ModItems.BORDER_SHARD.get()) {
+            int borderShardIncrease = CommonConfigs.BORDER_SHARD_INCREASE.get();
+            double blocksExpanded = pPlayer.getMainHandItem().getCount() * borderShardIncrease;
+            double newSize = border.getSize() + blocksExpanded;
+            border.lerpSizeBetween(border.getSize(), newSize, 1000);
+            data.setWorldBorderSize(newSize);
+            serverLevel.playSound(null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.BEACON_POWER_SELECT, SoundSource.BLOCKS, 0.75F, 0.9F);
+
+            if (!pPlayer.getAbilities().instabuild) {
+                pPlayer.getMainHandItem().setCount(0);
+            }
+            srv.getPlayerList().broadcastMessage(new TextComponent("[World Border] " + pPlayer.getDisplayName().getString() + " expanded the World border by " + blocksExpanded + " Blocks!").withStyle(ChatFormatting.YELLOW), ChatType.CHAT, Util.NIL_UUID);
+
+            } else {
+                double blocksRemoved = (double) pPlayer.getMainHandItem().getCount() * CommonConfigs.INVERTED_BORDER_SHARD_DECREASE.get();
+                double newSize = border.getSize() + blocksRemoved;
+                if (newSize > 5) {
+                    border.lerpSizeBetween(border.getSize(), newSize, 1000);
+                    data.setWorldBorderSize(newSize);
+                    serverLevel.playSound(null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.BEACON_POWER_SELECT, SoundSource.BLOCKS, 0.75F, 0.9F);
+
+                    if (!pPlayer.getAbilities().instabuild) {
+                        pPlayer.getMainHandItem().setCount(0);
                     }
+                    srv.getPlayerList().broadcastMessage(new TextComponent("[World Border] " + pPlayer.getDisplayName().getString() + " shrunk the World border by " + blocksRemoved + " Blocks!").withStyle(ChatFormatting.RED), ChatType.CHAT, Util.NIL_UUID);
 
-
-
-                    return InteractionResult.SUCCESS;
+                } else {
+                    srv.getPlayerList().broadcastMessage(new TextComponent("[World Border] " + pPlayer.getDisplayName().getString() + " attempted to shrink the border to less than 5 Blocks! ").withStyle(ChatFormatting.RED), ChatType.CHAT, Util.NIL_UUID);
                 }
             }
+            return InteractionResult.SUCCESS;
         }
-        return InteractionResult.PASS;
-    }
-
-
 }
