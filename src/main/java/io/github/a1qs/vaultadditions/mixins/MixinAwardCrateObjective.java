ec4b3ab57b6ed53a71971c21f138490df629937c
a1qs <ankiller2007.09.20@gmail.com>
2024-08-12 20:55:50 +0200

    Implement safe setting of the award Item upon completion

--- src/main/java/io/github/a1qs/vaultadditions/mixins/MixinAwardCrateObjective.java
+++ src/main/java/io/github/a1qs/vaultadditions/mixins/MixinAwardCrateObjective.java
@@ -1,5 +1,6 @@
 package io.github.a1qs.vaultadditions.mixins;
 
+import io.github.a1qs.vaultadditions.VaultAdditions;
 import io.github.a1qs.vaultadditions.config.CommonConfigs;
 import io.github.a1qs.vaultadditions.init.ModItems;
 import iskallia.vault.core.random.ChunkRandom;
@@ -13,15 +14,30 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
 @Mixin(value = AwardCrateObjective.class, remap = false)
 public class MixinAwardCrateObjective {
 
     @Inject(method = "awardCrate", at = @At("RETURN"))
     private void addBorderShardReward(Vault vault, Listener listener, ChunkRandom random, CallbackInfo ci) {
         StatCollector stats = vault.get(Vault.STATS).get(listener.get(Listener.ID));
+        int minAmount = CommonConfigs.VAULT_COMPLETION_GRANT_BORDER_SHARD_AMOUNT_MIN.get();
+        int maxAmount = CommonConfigs.VAULT_COMPLETION_GRANT_BORDER_SHARD_AMOUNT_MAX.get();
 
-        stats.get(StatCollector.REWARD).add(new ItemStack(ModItems.BORDER_SHARD.get(), random.nextInt(CommonConfigs.VAULT_COMPLETION_GRANT_BORDER_SHARD_AMOUNT_MIN.get(), CommonConfigs.VAULT_COMPLETION_GRANT_BORDER_SHARD_AMOUNT_MAX.get())));
+        int amount;
+        if (maxAmount < minAmount) {
+            VaultAdditions.LOGGER.error("Configured Max amount is lower than the Min amount for granting Border Shards. Swapping values.");
+
+            int temp = minAmount;
+            minAmount = maxAmount;
+            maxAmount = temp;
+        }
+
+        if (minAmount == maxAmount) {
+            amount = minAmount;
+        } else {
+            amount = random.nextInt(minAmount, maxAmount + 1);
+        }
+
+        stats.get(StatCollector.REWARD).add(new ItemStack(ModItems.BORDER_SHARD.get(), amount));
     }
-
 }
